e4859469cf55d4429a18b8a6b376cc21810c4743
Mike Guban <freemih@gmail.com>
2016-03-20 18:32:44 +0200

    Programming assignment done. Two functions 'makeCacheMatrix' and 'cacheSolve' edited

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,56 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## raw for inverted matrix. 
+## 'set' function for setting the value for x.
+## 'get' function for getting our matrix x. 
+## 'setInvertedMatrix' function for storing inverted matrix
+## 'getInvertedMatrix' function for obtaining cached inverted matrix.
 
+makeCacheMatrix <- function(x = matrix()) {
+  raw <- NULL
+  set <- function(param){
+    x <<- param
+    raw <<- NULL
+  }
+  
+  get <- function(){
+    x
+  }
+  
+  setInvertedMatrix <- function(invertedMatrix){
+    raw <<- invertedMatrix
+  }
+  
+  getInvertedMatrix <- function(){
+    raw
+  }
+  
+  data <- list(set = set, get = get, setInvertedMatrix = setInvertedMatrix, getInvertedMatrix = getInvertedMatrix)
+  matrix(data, length(data)/2, length(data)/2)
 }
 
 
 ## Write a short comment describing this function
+## First checking if there is a inverted matrix cached already.
+## If there is not, inverting matrix. Before inverting check the necessary conditions to be fulfilled.
+## After obtaining inverted matrix cache it.
+## Return the inverted matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  matr <- x$getInvertedMatrix()
+  if (!is.null(matr)){
+    message("Get cached inverted matrix")
+    return(matr)
+  }
+  
+  raw <- x$get()
+  ## Check our matrix for necessary conditions before inverting it
+  ## Our matrix 'x' has to be square matrix. And our matrix 'x' determinant must not be zero.
+  if (dim(raw)[1] == dim(raw)[2] & det(raw) != 0){
+    matr <- solve(raw, ...)
+  }
+  x$setInvertedMatrix(matr)
+  matr
 }
